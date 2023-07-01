5ea13546b3f5b97bc3f0c315229bf4cc625fe5e3
13alvone <13alvone@gmail.com>
2023-06-30 21:17:15 -0700

    ** Updated the install script after several test installs on my new, local, Kali Linux machine.

--- install.sh
+++ install.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-git clone https://github.com/13alvone/cspeakes_enumerator.git
-cd cspeakes_enumerator
 mv create_target.sh /usr/local/bin/
 chmod +x /usr/local/bin/create_target.sh
-echo 'alias target="/usr/local/bin/create_target.sh"' > ~/.profile
-source ~/.profile
+echo 'alias target="/usr/local/bin/create_target.sh"' > ~/.zshrc
+source ~/.zshrc
+
